ff40853869620313e03f52c9277046d32b1826a9
eduardo <171377664+barbosa-eduardo@users.noreply.github.com>
2024-07-19 09:53:38 -0300

    Add show_score function

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,9 +6,17 @@ require_relative 'board'
 class TicTacToe
   def initialize
     @game_board = Board.new
+    self.x_score = 0
+    self.o_score = 0
+  end
+
+  def show_score
+    puts "X: #{x_score} points"
+    puts "O: #{o_score} points"
   end
 
   private
 
   attr_reader :game_board
+  attr_accessor :x_score, :o_score
 end
