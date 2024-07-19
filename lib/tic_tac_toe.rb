125caf3a96939655fea616d9b8c541dcb54ed9a4
eduardo <171377664+barbosa-eduardo@users.noreply.github.com>
2024-07-19 09:47:21 -0300

    Initialize Tic Tac Toe class

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: false
 
+require_relative 'board'
+
 # Tic-Tac-Toe game
 class TicTacToe
+  def initialize
+    @game_board = Board.new
+  end
+
+  private
+
+  attr_reader :game_board
 end
--- main.rb
+++ main.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 
 require_relative 'lib/board'
-
-board = Board.new
-puts board
-board.mark('X', 0, 2)
-board.mark('O', 0, 2)
-puts board
+require_relative 'lib/tic_tac_toe'
