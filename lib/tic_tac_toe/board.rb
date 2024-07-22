dc3649f9c3454a5bbd0e33c606f26728bee6bced
eduardo <171377664+barbosa-eduardo@users.noreply.github.com>
2024-07-21 22:31:37 -0300

    Add won? method to Board

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -35,9 +35,26 @@ class Board
   end
 
   def won?
+    (0..2).each do |index|
+      row = board[index]
+      column = [board[0][index], board[1][index], board[2][index]]
+
+      return true if check(row)
+      return true if check(column)
+    end
+
+    diagonal_1 = [board[0][0], board[1][1], board[2][2]]
+    diagonal_2 = [board[0][2], board[1][1], board[2][0]]
+    check(diagonal_1) || check(diagonal_2)
   end
 
   def full?
+    board.each do |row|
+      row.each do |item|
+        return false if item.nil?
+      end
+    end
+    true
   end
 
   def available?(coord)
@@ -47,4 +64,12 @@ class Board
   private
 
   attr_accessor :board
+  attr_writer :winner
+
+  def check(array)
+    return false unless array.length == 3 && array.uniq == 1
+
+    self.winner = array[0]
+    true
+  end
 end
