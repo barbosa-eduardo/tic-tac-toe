30db55cad0b22564d300450b9d7aa67c805ba9b6
eduardo <171377664+barbosa-eduardo@users.noreply.github.com>
2024-07-23 09:08:24 -0300

    Fix input_coord

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,6 +13,13 @@ class TicTacToe
     @next_player = cross
   end
 
+  def play_round
+    self.board = Board.new
+    self.next_player = cross
+    play_turn while board.winner.nil? || board.full?
+    board.print
+  end
+
   def play_turn
     board.print
     coord = input_coord
@@ -22,11 +29,19 @@ class TicTacToe
 
   def input_coord
     puts 'Type the coordenates:'
-    puts 'Which row? (0/1/2)'
-    row = gets.chomp until row.length == 1 && row.ord.between?(48, 50)
+    row = String.new
+    loop do
+      puts 'Which row? (0/1/2)'
+      row = gets.chomp
+      break if row.length == 1 && row.ord.between?(48, 50)
+    end
 
-    puts 'Which column? (0/1/2)'
-    column = gets.chomp until column.length == 1 && column.ord.between?(48, 50)
+    column = String.new
+    loop do
+      puts 'Which column (0/1/2)'
+      column = gets.chomp
+      break if column.length == 1 && column.ord.between?(48, 50)
+    end
 
     [row.to_i, column.to_i]
   end
@@ -41,3 +56,6 @@ class TicTacToe
   attr_reader :board, :nought, :cross
   attr_writer :next_player
 end
+
+game = TicTacToe.new
+game.play_turn
